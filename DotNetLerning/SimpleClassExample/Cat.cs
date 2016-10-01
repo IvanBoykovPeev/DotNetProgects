1c70d35563dc8e701633ce65eaa3c057e27a623c
IvanPeev <ivanblade@outlook.com>
2016-10-01 18:52:27 +0300

    changes and add new class

---
+++ DotNetLerning/SimpleClassExample/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Cat
+    {
+        public string name;
+        public string color;
+    }
+}
