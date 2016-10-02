d4655d7d8ef9f54a98294f2499c78cf8cd43bcb1
IvanPeev <ivanblade@outlook.com>
2016-10-02 14:00:12 +0300

    example for delegate

---
+++ DotNetLerning/ExampleForDelegat/TestDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleForDelegat
+{
+    //Declaration of delegate
+    public delegate void SimpleDelegate(string aParam);
+
+    class TestDelegate
+    {
+        public static void TestFunction(string aParam)
+        {
+            Console.WriteLine("I was called by a delegate.");
+            Console.WriteLine("I got parameter {0}.", aParam);
+        }        
+
+        static void Main()
+        {
+            //Instantion of a delegate
+            SimpleDelegate simpleDelegate = new SimpleDelegate(TestFunction);
+            //Invocation of the method, pointed by a delegate
+            simpleDelegate("test");
+        }
+    }
+}
