4b54c3a9bb3e7d41f35a4a5777da318a7bef68ad
IvanPeev <ivanblade@outlook.com>
2016-10-05 17:10:54 +0300

    simple change

---
+++ DotNetLerning/SimpleClassExample/CatManipulating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class CatManipulating
+    {
+        public static void Main()
+        {
+            Cat Cat = new Cat();
+
+            Console.WriteLine("The color of cat {0} is {1}.", Cat.Name, Cat.Color);
+            Cat.SayMiau();
+
+            Cat myCat = new Cat("Susy", "White");
+
+            Console.WriteLine("The color of cat {0} is {1}.", myCat.Name, myCat.Color);
+            myCat.SayMiau();
+        }
+    }
+}
