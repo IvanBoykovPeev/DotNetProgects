ba14531683ec27263efb86e4e078ae6bd2b607c0
Ivan <ivanblade@outlook.com>
2016-10-09 17:33:40 +0300

    add Month program

--- DotNetLerning/Months/Properties/AssemblyInfo.cs
+++ DotNetLerning/Months/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Triangle")]
+[assembly: AssemblyTitle("Months")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Triangle")]
+[assembly: AssemblyProduct("Months")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a23a5292-74d1-4aff-a4a4-8908e9273d18")]
+[assembly: Guid("f8aea1f6-6670-4ab3-852f-1aa2f185b3dd")]
 
 // Version information for an assembly consists of the following four values:
 //
