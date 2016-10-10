00bba8f80ffec39078c60c37c1f791223a671898
ivan peev <ivanblade@outlook.com>
2016-10-10 13:34:01 +0300

    add SortingEngine

---
+++ DotNetLerning/SortingEngine/SortingEngine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingEngine
+{
+    class SortingEngine
+    {
+        static int[] Sort(params int[] numbers)
+        {
+            //The sorting logic
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                //Loop that in operating over the un-sorted part
+                //of the array
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    //Swaping the values
+                    if (numbers[i] > numbers[j])
+                    {
+                        int oldNumber = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = oldNumber;
+                    }
+                }
+            }//End of the sorting logic
+            return numbers;
+        }
+
+        static void PrintNumbers(params int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("{0}", numbers[i]);
+                if (i < (numbers.Length - 1))
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+
+
+        static void Main()
+        {
+            int[] numbers = { 10, 3, 5, -1, 0, 12, 8 };
+            Sort(numbers);
+            PrintNumbers(numbers);
+            Console.WriteLine();
+        }
+    }
+}
