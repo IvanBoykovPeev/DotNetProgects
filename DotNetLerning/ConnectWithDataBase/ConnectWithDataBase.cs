fdabda8d88b527c37a032a8a56d7261ead1038a4
ivan peev <ivanblade@outlook.com>
2016-10-12 12:21:57 +0300

    select name of products

--- DotNetLerning/ConnectWithDataBase/ConnectWithDataBase.cs
+++ DotNetLerning/ConnectWithDataBase/ConnectWithDataBase.cs
@@ -18,18 +18,40 @@ namespace ConnectWithDataBase
             SqlConnection con = new SqlConnection(CONNECTION_STRING);
             con.StateChange += new StateChangeEventHandler(ConnStateChanche);
             con.InfoMessage += new SqlInfoMessageEventHandler(InfoMessage);
-            //Open the conection
-            con.Open();
+
+            string qurryString = "SELECT Name from dbo.Products";
+
+            
             using (con)
             {
-                SqlCommand command = new SqlCommand("RAISERROR('Test error message', 6, 1)", con);
-                command.ExecuteNonQuery();
-            }
-            con.Close();
+                SqlCommand command = new SqlCommand(qurryString, con);
+
+                try
+                {
+                    //Open the conection
+                    con.Open();
+                    Console.WriteLine("--------------------------");
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0]);
+                    }
+
+                    Console.WriteLine("--------------------------");
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }            
         }
 
         private static void InfoMessage(object sender, SqlInfoMessageEventArgs e)
         {
+            Console.WriteLine("--------------------------------------");
             Console.WriteLine(e.Errors);
             Console.WriteLine(e.Message);
             Console.WriteLine(e.Source);
