f14cb47f808122ba1cf2e9cf683785a77df61daa
Ivan Peev <ivan.boykov.peev@gmail.com>
2016-10-13 13:54:50 +0300

    finali demodates

--- DotNetLerning/DatesDemo/DatesDemo.cs
+++ DotNetLerning/DatesDemo/DatesDemo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +20,49 @@ namespace DatesDemo
             demo.ConnectToSqlServer();
             demo.DropMessageTable();
             demo.CreateMessageTable();
+            demo.AddMessage("Test message 1", DateTime.Now);
+            demo.AddMessage("Test message 2", DateTime.Now);
+            demo.AddMessage("Test message 3", DateTime.Now);
+            CultureInfo cultureBulgaria = new CultureInfo("bg-BG");
+            demo.DisplayAllMessages(cultureBulgaria, "dd-MMMMMMM-yyyy HH:mm:ss");
+            demo.DropMessageTable();
             demo.DisconnectFromSqlServer();
+        }
+
+        private void DisplayAllMessages(CultureInfo aCultureInfo, string aFormat)
+        {
+            SqlCommand cmdSelectMsgs = new SqlCommand("SELECT MsgText, MsgDate FROM Message", mDbCon);
+            SqlDataReader reader = cmdSelectMsgs.ExecuteReader();
+            using (reader)
+            {
+                
+                while (reader.Read())
+                {
+                    string msgText = (string)reader["MsgText"];
+                    DateTime msgDate = (DateTime)reader["MsgDate"];
+                    string msgDateFormatted = msgDate.ToString(aFormat, aCultureInfo);
+                    Console.WriteLine("{0} - {1}", msgDateFormatted, msgText);
+                }
+            }
+        }
+
+        private void AddMessage(string aMsgText, DateTime aMsgDate)
+        {
+            SqlCommand cmdInsertMsg = new SqlCommand(
+                "INSERT INTO Message(MsgText, MsgDate) " +
+                "VALUES (@MsgText, @MsgDate)", mDbCon);
+
+            SqlParameter paramMsgText = new SqlParameter("@MsgText", SqlDbType.NVarChar);
+            paramMsgText.Value = aMsgText;
+            cmdInsertMsg.Parameters.Add(paramMsgText);
+
+            SqlParameter paramMsgDate = new SqlParameter("@MsgDate", SqlDbType.DateTime);
+            paramMsgDate.Value = aMsgDate;
+            cmdInsertMsg.Parameters.Add(paramMsgDate);
+
+            cmdInsertMsg.ExecuteNonQuery();
+
+            Console.WriteLine("Inserted record in Messages table.");
         }
 
         private void DropMessageTable()
@@ -36,7 +80,9 @@ namespace DatesDemo
             SqlCommand cmdCreateMsgTable = new SqlCommand(
                 @"CREATE TABLE Message
                 (
-                    MsgId int identity not null primary key
+                    MsgId int identity not null primary key,
+                    MsgText nvarchar(1000),
+                    MsgDate datetime
                 )",
                 mDbCon);
             cmdCreateMsgTable.ExecuteNonQuery();
