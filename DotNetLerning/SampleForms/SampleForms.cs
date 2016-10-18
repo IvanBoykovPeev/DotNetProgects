e99f58b6607852c3cb025a7733c658076998dc5f
Ivan Peev <ivan.boykov.peev@gmail.com>
2016-10-18 05:52:52 +0300

    add folder WindowsForms and program SampleForm

---
+++ DotNetLerning/SampleForms/SampleForms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SampleForms
+{
+    public class SampleForms : Form
+    {
+        static void Main()
+        {
+            SampleForms sampleForm = new SampleForms();
+            sampleForm.Text = "Sample form";
+            Button button = new Button();
+            button.Text = "Close";
+            button.Click += new EventHandler(sampleForm.button_Click);
+            sampleForm.Controls.Add(button);
+            sampleForm.ShowDialog();
+            sampleForm.Dispose();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
