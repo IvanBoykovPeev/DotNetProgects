c1d8e6cdde0f9b3b06cfc8cbcd5958e4c7b2de7e
Ivan Peev <ivan.boykov.peev@gmail.com>
2016-10-24 19:19:21 +0300

    Add program binding Control to list

---
+++ DotNetLerning/BindingControlToList/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BindingControlToList
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxTowns = new System.Windows.Forms.TextBox();
+            this.ButtonPrev = new System.Windows.Forms.Button();
+            this.ButtonNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBoxTowns
+            // 
+            this.TextBoxTowns.Location = new System.Drawing.Point(13, 13);
+            this.TextBoxTowns.Name = "TextBoxTowns";
+            this.TextBoxTowns.Size = new System.Drawing.Size(259, 20);
+            this.TextBoxTowns.TabIndex = 0;
+            // 
+            // ButtonPrev
+            // 
+            this.ButtonPrev.Location = new System.Drawing.Point(13, 40);
+            this.ButtonPrev.Name = "ButtonPrev";
+            this.ButtonPrev.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPrev.TabIndex = 1;
+            this.ButtonPrev.Text = "<< Prev";
+            this.ButtonPrev.UseVisualStyleBackColor = true;
+            this.ButtonPrev.Click += new System.EventHandler(this.ButtonPrev_Click);
+            // 
+            // ButtonNext
+            // 
+            this.ButtonNext.Location = new System.Drawing.Point(196, 39);
+            this.ButtonNext.Name = "ButtonNext";
+            this.ButtonNext.Size = new System.Drawing.Size(75, 23);
+            this.ButtonNext.TabIndex = 2;
+            this.ButtonNext.Text = "Next >>";
+            this.ButtonNext.UseVisualStyleBackColor = true;
+            this.ButtonNext.Click += new System.EventHandler(this.ButtonNext_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 80);
+            this.Controls.Add(this.ButtonNext);
+            this.Controls.Add(this.ButtonPrev);
+            this.Controls.Add(this.TextBoxTowns);
+            this.Name = "MainForm";
+            this.Text = "Binding Control To List";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxTowns;
+        private System.Windows.Forms.Button ButtonPrev;
+        private System.Windows.Forms.Button ButtonNext;
+    }
+}
---
+++ DotNetLerning/BindingControlToList/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BindingControlToList
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] towns = { "София", "Пловдив", "Варна", "Русе", "Бургас" };
+            TextBoxTowns.DataBindings.Add(new Binding("Text", towns, ""));
+        }
+
+        private void ButtonPrev_Click(object sender, EventArgs e)
+        {
+            CurrencyManager cm = (CurrencyManager)TextBoxTowns.DataBindings["Text"].BindingManagerBase;
+            if (cm.Position > 0)
+            {
+                cm.Position--;
+            }
+            else
+            {
+                cm.Position = cm.Count - 1;
+            }
+        }
+
+        private void ButtonNext_Click(object sender, EventArgs e)
+        {
+            CurrencyManager cm = (CurrencyManager)TextBoxTowns.DataBindings["Text"].BindingManagerBase;
+            if (cm.Position < cm.Count - 1)
+            {
+                cm.Position++;
+            }
+            else
+            {
+                cm.Position = 0;
+            }
+        }
+    }
+}
