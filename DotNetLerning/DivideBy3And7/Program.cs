dc218ef006d429da2a11b1730350d435deb7f3eb
Ivan Peev <ivan.boykov.peev@gmail.com>
2016-11-10 19:42:01 +0200

    Add exersice for loop

---
+++ DotNetLerning/DivideBy3And7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideBy3And7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter digit fron 1 to 100");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 0 || n > 100)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+
+
+                if (i % 21 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
