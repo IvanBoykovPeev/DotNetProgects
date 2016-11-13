03b830c735af4f729935cc3504f1440cd583f1aa
Ivan Peev <ivan.boykov.peev@gmail.com>
2016-11-13 17:52:17 +0200

    Add some loops example

---
+++ DotNetLerning/CalculateFactorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFactorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            //"decimal" is the biggest type that can hold intger values
+            decimal factorial = 1;
+            //Perform an "infinite loop"
+            while (true)
+            {
+                if (n <= 1)
+                {
+                    break;
+                }
+                factorial *= n;
+                n--;
+            }
+            Console.WriteLine("n! = " + factorial);
+        }
+    }
+}
