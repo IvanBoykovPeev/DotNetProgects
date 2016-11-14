de84601fd728c0daf238e071e52ebc0c2c9af8a8
Ivan Peev <ivan.boykov.peev@gmail.com>
2016-11-14 12:14:21 +0200

    Add CalculateFactorialWithDoWhileLoop example

---
+++ DotNetLerning/CalculateFactorialWithDoWhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFactorialWithDoWhileLoop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            decimal factorial = 1;
+            do
+            {
+                factorial *= n;
+                n--;
+            } while (n > 0);
+            Console.WriteLine("n! = " + factorial);
+        }
+    }
+}
