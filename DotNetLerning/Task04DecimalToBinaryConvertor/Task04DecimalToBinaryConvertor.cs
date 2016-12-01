edba0e5cc442d39c420801c5ce4ede45e4998252
ivan peev <ivan.boykov.peev@gmail.com>
2016-12-01 22:27:46 +0200

    Add Task04DecimalToBinaryConvertor

---
+++ DotNetLerning/Task04DecimalToBinaryConvertor/Task04DecimalToBinaryConvertor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04DecimalToBinaryConvertor
+{
+    class Task04DecimalToBinaryConvertor
+    {
+        static void Main()
+        {
+            string inputDecimalNumber = Console.ReadLine();
+            long decimalNumber = long.Parse(inputDecimalNumber);
+
+            if (decimalNumber < 0)
+            {
+                decimalNumber = decimalNumber ^ long.MinValue;
+            }
+
+            if (decimalNumber != 0)
+            {
+                string binaryRepresentation = null;
+                while (decimalNumber != 0)
+                {
+                    byte remainder = (byte)(decimalNumber % 2);
+                    binaryRepresentation = remainder + binaryRepresentation;
+                    decimalNumber /= 2;
+                }
+                Console.WriteLine(binaryRepresentation);
+            }
+
+            else
+            {
+                Console.WriteLine(decimalNumber);
+            }
+        }
+    }
+}
